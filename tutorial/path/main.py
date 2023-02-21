f4b7fe677eeecbe1ee847e4537c9d63625effe0e
verve-neowise <68016071+bekzod818@users.noreply.github.com>
2023-02-21 18:17:41 +0500

    path

--- tutorial/path/main.py
+++ tutorial/path/main.py
@@ -9,11 +9,27 @@ app = FastAPI(
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @app.get('/')
 async def get_index():
     return {"message": "Hello FastAPI!"}
+
+
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "resnet":
+        return {"model": model_name, "message": "LeCNN all the images"}
+    return {"model": model_name, "message": "Have some residuals!"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
 
 
 @app.get("/users")
