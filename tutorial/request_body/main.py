66fce103587c2f24873e562640996c1d47eceace
bekzod818 <bekzod030900@gmail.com>
2023-03-05 00:09:05 +0500

    fix: updated

--- tutorial/request_body/main.py
+++ tutorial/request_body/main.py
@@ -14,4 +14,16 @@ app = FastAPI()
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put('/items/{item_id}')
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, "item": item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
