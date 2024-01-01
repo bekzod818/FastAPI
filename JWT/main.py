261eaa7a8eec307678bcf6dcaedf8d6bb6e87bc2
bekzod818 <bekzod030900@gmail.com>
2024-01-02 00:10:40 +0500

    fix: jwt_requirements

---
+++ JWT/main.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+
+from database import SessionLocal, engine
+from fastapi import Depends, FastAPI, HTTPException, status
+from sqlalchemy.orm import Session
+
+import models
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: None, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    return {"User": user}
