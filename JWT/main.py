07812256d4653c701c4075e6f0b99960901fd10a
bekzod818 <bekzod030900@gmail.com>
2024-01-02 23:25:23 +0500

    add_logic

--- JWT/main.py
+++ JWT/main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 
+from auth import get_current_user, router
 from database import SessionLocal, engine
 from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
@@ -7,6 +8,7 @@ from sqlalchemy.orm import Session
 import models
 
 app = FastAPI()
+app.include_router(router=router)
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -20,10 +22,11 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
-async def user(user: None, db: db_dependency):
+async def user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication Failed")
     return {"User": user}
