9f870ebc139e0bb56fc8e93b485ebfc77305f54b
bekzod818 <bekzod030900@gmail.com>
2024-01-03 18:50:34 +0500

    fix: send_request_from_form

--- portfolio/main.py
+++ portfolio/main.py
@@ -1,9 +1,18 @@
 import os
 
+import aiohttp
+from environs import Env
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+env = Env()
+env.read_env()  # read .env file, if it exists
+# required variables
+BOT_TOKEN = env.str("BOT_TOKEN")  # => 'sloria'
+CHAT_ID = env.int("CHAT_ID")  # => raises error if not set
+
 
 app = FastAPI()
 
@@ -18,6 +27,25 @@ app.mount("/static", StaticFiles(directory=f"{base_dir}/static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+async def send_message(message):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+            data={"text": message, "chat_id": CHAT_ID},
+        ) as response:
+            return await response.json()
+
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/", response_class=HTMLResponse)
+async def post_root(request: Request):
+    response = await request.form()
+    name = response.get("name")
+    email = response.get("email")
+    message = f"Received Request\n\nName: {name}\nEmail: {email}"
+    await send_message(message=message)
     return templates.TemplateResponse("index.html", {"request": request})
