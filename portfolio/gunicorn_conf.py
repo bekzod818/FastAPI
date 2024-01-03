9bd1a9440f037eef59874b09b7bbc94a71e33261
bekzod818 <bekzod030900@gmail.com>
2024-01-03 13:22:38 +0500

    fix: updated

--- portfolio/gunicorn_conf.py
+++ portfolio/gunicorn_conf.py
@@ -1,13 +1,20 @@
+import os
 from multiprocessing import cpu_count
 
- # Socket Path
-bind = 'unix:/root/FastAPI/portfolio/gunicorn.sock'
+# Get the absolute path of the current script
+current_script_path = os.path.abspath(__file__)
 
- # Worker Options
+# Get the base directory (parent directory of the script)
+base_dir = os.path.dirname(current_script_path)
+
+# Socket Path
+bind = f"unix:{base_dir}/gunicorn.sock"
+
+# Worker Options
 workers = cpu_count() + 1
-worker_class = 'uvicorn.workers.UvicornWorker'
+worker_class = "uvicorn.workers.UvicornWorker"
 
- # Logging Options
-loglevel = 'debug'
-accesslog = '/root/FastAPI/portfolio/access_log'
-errorlog =  '/root/FastAPI/portfolio/error_log'
+# Logging Options
+loglevel = "debug"
+accesslog = "/root/FastAPI/portfolio/access_log"
+errorlog = "/root/FastAPI/portfolio/error_log"
--- portfolio/main.py
+++ portfolio/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -5,7 +7,13 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# Get the absolute path of the current script
+current_script_path = os.path.abspath(__file__)
+
+# Get the base directory (parent directory of the script)
+base_dir = os.path.dirname(current_script_path)
+
+app.mount("/static", StaticFiles(directory=f"{base_dir}/static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
