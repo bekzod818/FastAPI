05f7e46d60210e5d16add527a6b3f469ee1448fe
bekzod818 <bekzod030900@gmail.com>
2024-02-20 23:34:05 +0500

    fix: Dockerfile

--- Comprehensive_Guide_JWT/main.py
+++ Comprehensive_Guide_JWT/main.py
@@ -1,4 +1,5 @@
 import fastapi
+import uvicorn
 
 app = fastapi.FastAPI()
 
@@ -16,3 +17,7 @@ def login():
     - password:
     """
     return "ThisTokenIsFake"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
